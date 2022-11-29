d9b7078cd395c3e0f5baa2f05a09ef2e117cc468
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-11-29 19:52:51 +0200

    c5 commit

--- cursor/src/main/java/com/cursor/cursor/web/UserRestController.java
+++ cursor/src/main/java/com/cursor/cursor/web/UserRestController.java
@@ -1,4 +1,15 @@
 package com.cursor.cursor.web;
 
+import com.cursor.cursor.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users/")
+@RequiredArgsConstructor
 public class UserRestController {
+
+    private final UserService userService;
+
 }
--- cursor/src/main/java/com/cursor/cursor/web/UserSingUpController.java
+++ cursor/src/main/java/com/cursor/cursor/web/UserSingUpController.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/sign-up")
+@RequestMapping("/sign-up")
 @RequiredArgsConstructor
 public class UserSingUpController {
 
