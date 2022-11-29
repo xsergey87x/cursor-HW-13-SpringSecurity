50a146b152bee31fd0b11b2b93e9a2c9f49f06d5
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-11-29 19:48:09 +0200

    c3 commit

--- cursor/src/main/java/com/cursor/cursor/service/dto/LoginDto.java
+++ cursor/src/main/java/com/cursor/cursor/service/dto/LoginDto.java
@@ -1,4 +1,9 @@
 package com.cursor.cursor.service.dto;
 
+import lombok.Data;
+
+@Data
 public class LoginDto {
+    private String username;
+    private String password;
 }
--- cursor/src/main/java/com/cursor/cursor/service/exception/UserNotExistException.java
+++ cursor/src/main/java/com/cursor/cursor/service/exception/UserNotExistException.java
@@ -1,4 +1,12 @@
 package com.cursor.cursor.service.exception;
 
-public class UserNotExistException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class UserNotExistException extends RuntimeException{
+
+    public UserNotExistException(String message) {
+        super(message);
+    }
 }
