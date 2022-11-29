205ed341d1163170269eb1bbb27c2d574451b004
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-11-29 19:44:47 +0200

    c2 commit

--- cursor/src/main/java/com/cursor/cursor/service/UserService.java
+++ cursor/src/main/java/com/cursor/cursor/service/UserService.java
@@ -1,4 +1,13 @@
 package com.cursor.cursor.service;
 
+import com.cursor.cursor.entity.User;
+
+import java.util.List;
+
 public interface UserService {
+
+    User saveUser(User user);
+    void deleteUser(User user);
+    User getByUsername(String username);
+    List<User> getAllUser();
 }
--- cursor/src/main/java/com/cursor/cursor/service/implementation/UserServiceImpl.java
+++ cursor/src/main/java/com/cursor/cursor/service/implementation/UserServiceImpl.java
@@ -1,4 +1,28 @@
 package com.cursor.cursor.service.implementation;
 
-public class UserServiceImpl {
+import com.cursor.cursor.entity.User;
+import com.cursor.cursor.service.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User saveUser(User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(User user) {
+
+    }
+
+    @Override
+    public User getByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUser() {
+        return null;
+    }
 }
