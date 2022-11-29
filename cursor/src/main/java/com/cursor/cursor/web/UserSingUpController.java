7c5ba84ea6c9386240a25fc405529ce72d12750f
Tytyanchuk Sergey <xsergey87x@ukr.net>
2022-11-29 19:51:20 +0200

    c4 commit

--- cursor/src/main/java/com/cursor/cursor/web/UserSingUpController.java
+++ cursor/src/main/java/com/cursor/cursor/web/UserSingUpController.java
@@ -1,4 +1,22 @@
 package com.cursor.cursor.web;
 
+import com.cursor.cursor.entity.User;
+import com.cursor.cursor.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/sign-up")
+@RequiredArgsConstructor
 public class UserSingUpController {
+
+    private final UserService userService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User signUp(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
 }
