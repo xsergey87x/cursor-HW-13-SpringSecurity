be4e2d5e80ae0abb6c8fd0dcbd92cdf3677df3f7
Sergey <xsergey87x@ukr.net>
2022-12-01 18:14:47 +0200

    cc1

--- cursor/src/main/java/com/cursor/cursor/configuration/WebSecurity.java
+++ cursor/src/main/java/com/cursor/cursor/configuration/WebSecurity.java
@@ -1,4 +1,17 @@
 package com.cursor.cursor.configuration;
 
-public class WebSecurity {
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/img/**").permitAll()
+                .anyRequest().authenticated();
+    }
+
 }
